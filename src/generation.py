d0296c68848f9ce8d040c9bc1322fbec1a494f0e
TechTastic <thetechnomancer123@gmail.com>
2025-03-12 21:58:56 -0500

    Ch5 L4

--- src/generation.py
+++ src/generation.py
@@ -12,12 +12,7 @@ def generate_page(from_path, template_path, dest_path):
     markdown_file = open(from_path, "r").read()
     template_file = open(template_path, "r").read()
 
-    print("======== Markdown ========")
-    print(markdown_file)
-
     content = markdown_to_html_node(markdown_file).to_html()
-    print("======== HTML ========")
-    print(content)
     title = extract_title(markdown_file)
     
     final_html = template_file.replace(r"{{ Title }}", title).replace(r"{{ Content }}", content)
